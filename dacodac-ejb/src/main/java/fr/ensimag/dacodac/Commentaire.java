c244a34a5c9e67b562a4e4aa9a32e327f44436b9
Weschler Leo <leo.weschler@ensimag.grenoble-inp.fr>
2017-02-06 09:48:28 +0100

    Facade creation

--- dacodac-ejb/src/main/java/fr/ensimag/dacodac/Commentaire.java
+++ dacodac-ejb/src/main/java/fr/ensimag/dacodac/Commentaire.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 /**
@@ -23,6 +24,7 @@ public class Commentaire implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @ManyToOne
     private Utilisateur auteur;
     private LocalDateTime dateCreation;
     private String titreAnnonce;
