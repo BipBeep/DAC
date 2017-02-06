4d30a0dd1eb83ad3c5ae854dbfa60e88ff5d0ffe
Rémi Duclau <remi.duclau@ensimag.grenoble-inp.fr>
2017-02-06 09:21:36 +0100

    annonce

---
+++ dacodac-ejb/src/main/java/fr/ensimag/dacodac/Annonce.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ensimag.dacodac;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author duclaur
+ */
+@Entity
+public class Annonce implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private int prix;
+    
+    private Utilisateur auteur;
+
+    /**
+     * Get the value of auteur
+     *
+     * @return the value of auteur
+     */
+    public Utilisateur getAuteur() {
+        return auteur;
+    }
+
+    /**
+     * Set the value of auteur
+     *
+     * @param auteur new value of auteur
+     */
+    public void setAuteur(Utilisateur auteur) {
+        this.auteur = auteur;
+    }
+
+
+    /**
+     * Get the value of prix
+     *
+     * @return the value of prix
+     */
+    public int getPrix() {
+        return prix;
+    }
+
+    /**
+     * Set the value of prix
+     *
+     * @param prix new value of prix
+     */
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Annonce)) {
+            return false;
+        }
+        Annonce other = (Annonce) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "fr.ensimag.dacodac.Annonce[ id=" + id + " ]";
+    }
+    
+}
