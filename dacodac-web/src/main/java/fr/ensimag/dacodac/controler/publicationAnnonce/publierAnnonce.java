9946495615310c701bc496c23fe33f45c8a602c5
mignotju <justine.mignot@ensimag.grenoble-inp.fr>
2017-02-07 11:16:40 +0100

    vue publication annonce

---
+++ dacodac-web/src/main/java/fr/ensimag/dacodac/controler/publicationAnnonce/publierAnnonce.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ensimag.dacodac.controler.publicationAnnonce;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author mignotju
+ */
+@Named(value = "publierAnnonce")
+@Dependent
+public class publierAnnonce {
+
+    /**
+     * Creates a new instance of publierAnnonce
+     */
+    public publierAnnonce() {
+    }
+    
+    private String tags;
+    private String titre;
+    private String description;
+    private String type;
+    private String prix;
+    private String codePostal;
+ 
+    public String getTags() {
+        return tags;
+    }
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+ 
+    public String getTitre() {
+        return titre;
+    }
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public String getPrix() {
+        return prix;
+    }
+    public void setPrix(String prix) {
+        this.prix = prix;
+    }
+    
+    public String getCodePostal() {
+        return codePostal;
+    }
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+    
+}
