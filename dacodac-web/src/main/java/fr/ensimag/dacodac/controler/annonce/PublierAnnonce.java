d0d32efff6a8109c19e3b444c7f27760678fdce6
Jonas Maubert <jonas.maubert@ensimag.grenoble-inp.fr>
2017-02-08 17:26:46 +0100

    rajout des tags dans la creation d annonce

--- dacodac-web/src/main/java/fr/ensimag/dacodac/controler/annonce/PublierAnnonce.java
+++ dacodac-web/src/main/java/fr/ensimag/dacodac/controler/annonce/PublierAnnonce.java
@@ -6,12 +6,15 @@
 package fr.ensimag.dacodac.controler.annonce;
 
 import fr.ensimag.dacodac.Annonce;
+import fr.ensimag.dacodac.Tag;
 import fr.ensimag.dacodac.TypeAnnonce;
 import fr.ensimag.dacodac.Utilisateur;
 import fr.ensimag.dacodac.stateless.AnnonceFacadeLocal;
 import fr.ensimag.dacodac.stateless.UtilisateurFacadeLocal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
@@ -38,6 +41,7 @@ public class PublierAnnonce {
     
     private Annonce annonce;
     private Utilisateur utilisateur;
+    private String tags;
     
     public Annonce getAnnonce() {
         return annonce;
@@ -45,6 +49,11 @@ public class PublierAnnonce {
     
     public String save() {
         annonceFacade.create(annonce);
+        String[] arrayTags = tags.split(" ");
+        for (String s : arrayTags) {
+            annonceFacade.addTag(annonce, new Tag(s));
+        }
+        annonceFacade.edit(annonce);
         utilisateurFacade.addAnnonce(utilisateur, annonce);
         utilisateurFacade.edit(utilisateur);
         return "index.xhtml";
