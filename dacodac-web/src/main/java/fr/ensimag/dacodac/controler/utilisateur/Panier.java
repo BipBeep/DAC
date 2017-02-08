7ce544b3677fc1d9e32a88a6ad7d2dff8d863955
Jonas Maubert <jonas.maubert@ensimag.grenoble-inp.fr>
2017-02-08 16:20:36 +0100

    controlleur pour le panier et la modification e profil

---
+++ dacodac-web/src/main/java/fr/ensimag/dacodac/controler/utilisateur/Panier.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ensimag.dacodac.controler.utilisateur;
+
+import fr.ensimag.dacodac.Annonce;
+import fr.ensimag.dacodac.Utilisateur;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.context.SessionScoped;
+
+/**
+ *
+ * @author maubertj
+ */
+@Named(value = "panier")
+@SessionScoped
+public class Panier implements Serializable {
+
+    
+    private List<Annonce> annonces; 
+    
+    /**
+     * Creates a new instance of Panier
+     */
+    public Panier() {
+        annonces = new ArrayList<>();
+    }
+    
+    
+    public List<Annonce> getAnnonces() {
+        return annonces;
+    }
+    
+    public void addAnnonce(Annonce annonce) {
+        annonces.add(annonce);
+    }
+    
+    public void removeAnnonce(Annonce annonce) {
+        annonces.remove(annonce);
+    }
+    
+}
