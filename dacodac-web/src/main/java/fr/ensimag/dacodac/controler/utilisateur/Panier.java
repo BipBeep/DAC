a46176db59334fc2877aeeabe1dda6583fa2da70
Jonas Maubert <jonas.maubert@ensimag.grenoble-inp.fr>
2017-02-09 10:54:46 +0100

    ajoute au panier sur les annonces ajoute maintenant au panier

--- dacodac-web/src/main/java/fr/ensimag/dacodac/controler/utilisateur/Panier.java
+++ dacodac-web/src/main/java/fr/ensimag/dacodac/controler/utilisateur/Panier.java
@@ -7,12 +7,16 @@ package fr.ensimag.dacodac.controler.utilisateur;
 
 import fr.ensimag.dacodac.Annonce;
 import fr.ensimag.dacodac.Utilisateur;
+import fr.ensimag.dacodac.stateless.AnnonceFacadeLocal;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
 /**
  *
@@ -22,29 +26,62 @@ import javax.enterprise.context.SessionScoped;
 @SessionScoped
 public class Panier implements Serializable {
 
-    private List<Annonce> annonces = null;
+    @EJB(name = "annonceFacade")
+    private AnnonceFacadeLocal annonceFacade;
+
+    @Inject
+    private Identification beanID;
+
+    private Set<Annonce> annonces = null;
+
+    public void setIdentification(Identification identification) {
+        this.beanID = identification;
+    }
+
+    public Identification getIdentification() {
+        return beanID;
+    }
 
     /**
      * Creates a new instance of Panier
      */
     public Panier() {
-        annonces = new ArrayList<>();
+        annonces = new HashSet<>();
     }
 
-    public List<Annonce> getAnnonces() {
+    public Set<Annonce> getAnnonces() {
         if (annonces == null) {
-            annonces = new ArrayList<>();
+            annonces = new HashSet<>();
         }
         return annonces;
 
     }
 
     public void addAnnonce(Annonce annonce) {
-        annonces.add(annonce);
+        String msg;
+        if(annonces.add(annonce)) {
+            msg = "Cette annonce a été ajouté à votre panier";
+        } else {
+            msg = "Cette annonce est déjà présente dans votre panier";
+        }
+        
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, msg, null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println(annonces.toString());
     }
 
     public void removeAnnonce(Annonce annonce) {
         annonces.remove(annonce);
+    }
+
+    public String validerPanier() {
+        Utilisateur u = beanID.getIdentite();
+        for (Annonce a : annonces) {
+            annonceFacade.addPostulant(a, u);
+            annonceFacade.edit(a);
+        }
+        return "index.xhtml";
     }
 
 }
