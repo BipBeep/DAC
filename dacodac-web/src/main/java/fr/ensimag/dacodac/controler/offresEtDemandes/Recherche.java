ab3522b6aa40676d2981989af370843c37a5033d
mignotju <justine.mignot@ensimag.grenoble-inp.fr>
2017-02-10 12:15:30 +0100

    larecherche est au taquet

--- dacodac-web/src/main/java/fr/ensimag/dacodac/controler/offresEtDemandes/Recherche.java
+++ dacodac-web/src/main/java/fr/ensimag/dacodac/controler/offresEtDemandes/Recherche.java
@@ -27,10 +27,10 @@ import javax.enterprise.context.RequestScoped;
 @Named(value = "recherche")
 @RequestScoped
 public class Recherche {
-    
+
     @EJB
     private AnnonceFacadeLocal annonceFacade;
-    
+
     @EJB
     private TagFacadeLocal tagFacade;
 
@@ -39,11 +39,10 @@ public class Recherche {
     String tags;
     private List<Annonce> offres = null;
     private List<Annonce> demandes = null;
-    
-    
-    String tab_depart[] = {"ain","aisne","allier","hautes-alpes","alpes-de-haute-provence","alpes-maritimes","ardeche","ardennes","ariege","aube","aude","aveyron","bouches-du-rhone","calvados","cantal","charente","charente-maritime","cher","correze","corse-du-sud","haute-corse","cote-dor","cotes-darmor","creuse","dordogne","doubs","drome","eure","eure-et-loir","finistere","gard","haute-garonne","gers","gironde","herault","ile-et-vilaine","indre","indre-et-loire","isere","jura","landes","loir-et-cher","loire","haute-loire","loire-atlantique","loiret","lot","lot-et-garonne","lozere","maine-et-loire","manche","marne","haute-marne","mayenne","meurthe-et-moselle","meuse","morbihan","moselle","nievre","nord","oise","orne","pas-de-calais","puy-de-dome","pyrenees-atlantiques","hautes-pyrenees","pyrenees-orientales","bas-rhin","haut-rhin","rhone","haute-saone","saone-et-loire","sarthe","savoie","haute-savoie","paris","seine-maritime","seine-et-marne","yvelines","deux-sevres","somme","tarn","tarn-et-garonne","var","vaucluse","vendee","vienne","haute-vienne","vosges","yonne","territoire-de-belfort","essonne","hauts-de-seine","seine-saint-denis","val-de-marne","val-doise","mayotte","guadeloupe","guyane","martinique","reunion"};
-    private Map<String,String> liste_departs;
-    
+
+    String tab_depart[] = {"ain", "aisne", "allier", "hautes-alpes", "alpes-de-haute-provence", "alpes-maritimes", "ardeche", "ardennes", "ariege", "aube", "aude", "aveyron", "bouches-du-rhone", "calvados", "cantal", "charente", "charente-maritime", "cher", "correze", "corse-du-sud", "haute-corse", "cote-dor", "cotes-darmor", "creuse", "dordogne", "doubs", "drome", "eure", "eure-et-loir", "finistere", "gard", "haute-garonne", "gers", "gironde", "herault", "ile-et-vilaine", "indre", "indre-et-loire", "isere", "jura", "landes", "loir-et-cher", "loire", "haute-loire", "loire-atlantique", "loiret", "lot", "lot-et-garonne", "lozere", "maine-et-loire", "manche", "marne", "haute-marne", "mayenne", "meurthe-et-moselle", "meuse", "morbihan", "moselle", "nievre", "nord", "oise", "orne", "pas-de-calais", "puy-de-dome", "pyrenees-atlantiques", "hautes-pyrenees", "pyrenees-orientales", "bas-rhin", "haut-rhin", "rhone", "haute-saone", "saone-et-loire", "sarthe", "savoie", "haute-savoie", "paris", "seine-maritime", "seine-et-marne", "yvelines", "deux-sevres", "somme", "tarn", "tarn-et-garonne", "var", "vaucluse", "vendee", "vienne", "haute-vienne", "vosges", "yonne", "territoire-de-belfort", "essonne", "hauts-de-seine", "seine-saint-denis", "val-de-marne", "val-doise", "mayotte", "guadeloupe", "guyane", "martinique", "reunion"};
+    private Map<String, String> liste_departs;
+
     Comparator<String> keyComparator = new Comparator<String>() {
         @Override
         public int compare(String s1, String s2) {
@@ -60,7 +59,7 @@ public class Recherche {
             }
         }
     };
-    
+
     /**
      * Creates a new instance of Recherche
      */
@@ -72,10 +71,10 @@ public class Recherche {
             num = Integer.toString(i);
             code = num.concat(" - ");
             num_dep = code.concat(tab_depart[i]);
-            liste_departs.put(num_dep,num_dep);
+            liste_departs.put(num_dep, num_dep);
         }
     }
-    
+
     public String effectuer(String type) {
         TypeAnnonce typeA = TypeAnnonce.OFFRE;
         if (type.equals("Demandes")) {
@@ -90,69 +89,113 @@ public class Recherche {
                 listTags.add(t);
             }
         }
-        
+
         //Departement
-        String codeDepart = "";
+        String departChoisi = "";
         if (depart != null) {
-            codeDepart = depart.split(" ")[0];
+            departChoisi = depart.split(" ")[0];
         }
-        
+
+        //tri
+        Comparator<Annonce> comparateurAnnonce = new Comparator<Annonce>() {
+            @Override
+            public int compare(Annonce a1, Annonce a2) {
+                // si aucun tri sélectionné, prix croissants par défaut
+                if (tri == null || tri.equals("prix_croissants")) {
+                    if (a1.getPrix() < a2.getPrix()) {
+                        return -1;
+                    } else if (a1.getPrix() == a2.getPrix()) {
+                        return 0;
+                    } else {
+                        return 1;
+                    }
+                } else if (tri.equals("prix_decroissants")) {
+                    if (a1.getPrix() > a2.getPrix()) {
+                        return -1;
+                    } else if (a1.getPrix() == a2.getPrix()) {
+                        return 0;
+                    } else {
+                        return 1;
+                    }
+                } else if (tri.equals("dates_croissantes")) {
+                    return -a1.getDatePublication().compareTo(a2.getDatePublication());
+                } else if (tri.equals("dates_decroissantes")) {
+                    return a1.getDatePublication().compareTo(a2.getDatePublication());
+                } else {
+                    // never
+                    return 0;
+                }
+            }
+        };
+
         if (type.equals("Demandes")) {
-            demandes = annonceFacade.findDemandesByTagsAndDepartement(listTags, codeDepart);
+            demandes = annonceFacade.findDemandesByTagsAndDepartement(listTags, departChoisi);
+            if (demandes != null) {
+                demandes.sort(comparateurAnnonce);
+            }
             return "demandes.xhtml";
         } else {
-            offres = annonceFacade.findOffresByTagsAndDepartement(listTags, codeDepart);
+            offres = annonceFacade.findOffresByTagsAndDepartement(listTags, departChoisi);
+            if (offres != null) {
+                offres.sort(comparateurAnnonce);
+            }
             return "offres.xhtml";
         }
 
     }
-    
+
     public String getDepartement() {
         return depart;
     }
-    public void setDepartement(String depart ) {
+
+    public void setDepartement(String depart) {
         this.depart = depart;
     }
-    
+
     public String getTri() {
         return tri;
     }
+
     public void setTri(String tri) {
         this.tri = tri;
     }
-    
-    public Map<String,String> getDepartements() {
+
+    public Map<String, String> getDepartements() {
         return liste_departs;
     }
-    public void setDepartements(Map<String,String> departs) {
+
+    public void setDepartements(Map<String, String> departs) {
         this.liste_departs = departs;
     }
-    
+
     public String getTags() {
         return tags;
     }
+
     public void setTags(String tags) {
         this.tags = tags;
     }
-    
-    public List<Annonce> getOffres(){
-        if(offres==null){
+
+    public List<Annonce> getOffres() {
+        if (offres == null) {
             offres = ensembleOffres();
         }
         return offres;
     }
+
     public List<Annonce> ensembleOffres() {
         return annonceFacade.findLatest(5, TypeAnnonce.OFFRE);
     }
-    
-    public List<Annonce> getDemandes(){
-        if(demandes==null){
+
+    public List<Annonce> getDemandes() {
+        if (demandes == null) {
             demandes = ensembleDemandes();
         }
         return demandes;
     }
+
     public List<Annonce> ensembleDemandes() {
         return annonceFacade.findLatest(5, TypeAnnonce.DEMANDE);
     }
-    
+
 }
