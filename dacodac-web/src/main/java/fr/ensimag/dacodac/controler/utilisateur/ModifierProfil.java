1c6fd820544c8ba8979c517cc5572101911e2e5d
KamoKLM <nicolas.kamowski@ensimag.grenoble-inp.fr>
2017-02-10 13:44:41 +0100

    modif qui ne marche pas

--- dacodac-web/src/main/java/fr/ensimag/dacodac/controler/utilisateur/ModifierProfil.java
+++ dacodac-web/src/main/java/fr/ensimag/dacodac/controler/utilisateur/ModifierProfil.java
@@ -7,26 +7,31 @@ package fr.ensimag.dacodac.controler.utilisateur;
 
 import fr.ensimag.dacodac.Utilisateur;
 import fr.ensimag.dacodac.stateless.UtilisateurFacadeLocal;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.inject.Named;
-import javax.enterprise.context.Dependent;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 
 /**
  *
  * @author maubertj
  */
 @Named(value = "modifierProfil")
-@RequestScoped
+@ViewScoped
 public class ModifierProfil {
 
     @EJB
     private UtilisateurFacadeLocal utilisateurFacade;
-
+    
+    private String pseudo;
+    
     private Utilisateur utilisateur = null;
 
-    private String nouveauMotDePasse;
-    private String ancienMotDePasse;
+    private String nouveauMotDePasse = null;
+    
+    private String ancienMotDePasse = null;
 
     /**
      * Creates a new instance of ModifierProfil
@@ -34,26 +39,48 @@ public class ModifierProfil {
     public ModifierProfil() {
     }
 
+    @PostConstruct
+    public void postBuild() {
+        pseudo = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("pseudo");
+        utilisateur = utilisateurFacade.findByPseudo(pseudo);
+    }
+
     public String modificationProfil() {
         //Gestion du mot de passe
-        if ((!nouveauMotDePasse.equals("")) && ancienMotDePasse.equals(utilisateur.getPassword())) {
+        /*if ((!nouveauMotDePasse.equals(null)) && ancienMotDePasse.equals(utilisateur.getPassword())) {
+            System.err.println("I'm in!");
             utilisateur.setPassword(nouveauMotDePasse);
-        }
+        }*/
         utilisateurFacade.edit(utilisateur);
         return "index.xhtml";
     }
 
     public String getNouveauMotDePasse() {
+        if (nouveauMotDePasse == null) {
+            nouveauMotDePasse = new String();
+        }
         return nouveauMotDePasse;
     }
 
+    public void setNouveauMotDePasse(String nouveauMotDePasse) {
+        this.nouveauMotDePasse = nouveauMotDePasse;
+    }
+
+    public void setAncienMotDePasse(String ancienMotDePasse) {
+        this.ancienMotDePasse = ancienMotDePasse;
+    }
+
     public String getAncienMotDePasse() {
+        if (ancienMotDePasse == null) {
+            ancienMotDePasse = new String();
+        }
         return ancienMotDePasse;
     }
 
-    public Utilisateur getUtilisateur(String pseudo) {
+    public Utilisateur getUtilisateur() {
         if (utilisateur == null) {
-            utilisateur = utilisateurFacade.findByPseudo(pseudo);
+            utilisateur = new Utilisateur();
+            utilisateur.setPseudo(pseudo);
         }
         return utilisateur;
     }
