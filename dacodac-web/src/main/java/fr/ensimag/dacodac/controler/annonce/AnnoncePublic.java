e5b03dd1bf5d3691ea8beec5c0acde563fdc4eec
Weschler Leo <leo.weschler@ensimag.grenoble-inp.fr>
2017-02-14 21:59:08 +0100

    Dakos enlever ?

--- dacodac-web/src/main/java/fr/ensimag/dacodac/controler/annonce/AnnoncePublic.java
+++ dacodac-web/src/main/java/fr/ensimag/dacodac/controler/annonce/AnnoncePublic.java
@@ -6,11 +6,16 @@
 package fr.ensimag.dacodac.controler.annonce;
 
 import fr.ensimag.dacodac.Annonce;
+import fr.ensimag.dacodac.Commentaire;
+import fr.ensimag.dacodac.TypeAnnonce;
 import fr.ensimag.dacodac.Utilisateur;
 import fr.ensimag.dacodac.controler.commentaire.LaisserCommentaire;
 import fr.ensimag.dacodac.controler.utilisateur.Identification;
 import fr.ensimag.dacodac.stateless.AnnonceFacadeLocal;
+import fr.ensimag.dacodac.stateless.CommentaireFacadeLocal;
+import fr.ensimag.dacodac.stateless.UtilisateurFacadeLocal;
 import java.io.Serializable;
+import java.time.LocalDate;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -31,8 +36,15 @@ public class AnnoncePublic implements Serializable {
 
     @EJB
     private AnnonceFacadeLocal annonceFacade;
+    @EJB
+    private UtilisateurFacadeLocal utilisateurFacade;
+    @EJB
+    private CommentaireFacadeLocal commentaireFacade;
 
+    private Utilisateur destinataire = null;
     private Annonce annonce = null;
+
+    private String description = "Commentaire ...";
 
     /**
      * Creates a new instance of AnnoncePublic
@@ -46,6 +58,30 @@ public class AnnoncePublic implements Serializable {
 
     public Identification getIdentification() {
         return beanID;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDestinataire(Utilisateur destinataire) {
+        this.destinataire = destinataire;
+    }
+
+    public Utilisateur getDestinataire() {
+        return destinataire;
+    }
+
+    public void setAnnonce(Annonce annonce) {
+        this.annonce = annonce;
+    }
+
+    public Annonce getAnnonce() {
+        return annonce;
     }
 
     public Annonce getAnnonce(long id) {
@@ -70,13 +106,35 @@ public class AnnoncePublic implements Serializable {
     public String serviceRendu() {
         if (beanID.getIdentite().equals(annonce.getAuteur())) {
             annonce.setServiceRendu_auteur(true);
+            destinataire = annonce.getAuteur();
         } else {
             annonce.setServiceRendu_contracteur(true);
+            destinataire = annonce.getPostulants().get(0);
         }
         annonceFacade.edit(annonce);
         if (annonce.getServiceRendu_auteur() && annonce.getServiceRendu_contracteur()) {
+            int prix = annonce.getPrix();
+            if (annonce.getType() == TypeAnnonce.DEMANDE) {
+                annonce.getAuteur().setDakos(annonce.getAuteur().getDakos() - prix);
+                annonce.getPostulants().get(0).setDakos(annonce.getPostulants().get(0).getDakos() + prix);
+            } else {
+                annonce.getAuteur().setDakos(annonce.getAuteur().getDakos() + prix);
+                annonce.getPostulants().get(0).setDakos(annonce.getPostulants().get(0).getDakos() - prix);
+            }
+            utilisateurFacade.edit(annonce.getAuteur());
+            utilisateurFacade.edit(annonce.getPostulants().get(0));
             annonceFacade.remove(annonce);
         }
+        return "index.xhtml";
+    }
+
+    public String save() {
+        Utilisateur auteur = beanID.getIdentite();
+
+        Commentaire commentaire = new Commentaire(auteur, destinataire, LocalDate.now(), description, annonce.getTitre());
+        commentaireFacade.create(commentaire);
+        utilisateurFacade.addCommentaire(commentaire);
+
         return "index.xhtml";
     }
 
